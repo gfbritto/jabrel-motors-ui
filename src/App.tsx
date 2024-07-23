642c3945ba6be9139c733bbd6628508c12c5aabd
Gabriel Brito <gfbritto@outlook.com>
2024-07-22 21:28:39 -0300

    feat: add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+          Actions
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Download</MenuItem>
+          <MenuItem>Create a Copy</MenuItem>
+          <MenuItem>Mark as Draft</MenuItem>
+          <MenuItem>Delete</MenuItem>
+          <MenuItem>Attend a Workshop</MenuItem>
+        </MenuList>
+      </Menu>
+    </>
+  );
 }
 
 export default App;
